d80ecb39433d2b428fa03512c996fd1fcbf0051b
Abdul Rehman Aziz <abdulrehmanx911@gmail.com>
2022-08-10 14:17:16 +0500

    cleaned and changed file structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import ContactUs from "./components/ContactUs";
+import Home from "./components/Essentials/Home/Home";
+import About from "./components/Essentials/About/About";
+import ContactUs from "./components/Essentials/ContactUs/ContactUs";
 import Attires from "./components/Attire/Attires";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
-const App:React.FC = () =>{
+import Navbar from "./components/Essentials/Navbar/Navbar";
+import Footer from "./components/Essentials/Footer/Footer";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import NotFound from "./components/Essentials/NotFound/NotFound";
+const App: React.FC = () => {
   return (
     <>
       <BrowserRouter>
@@ -18,14 +20,17 @@ const App:React.FC = () =>{
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/attires" element={<Attires />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
   );
-}
+};
 
 //added comment by daniyal
-
 
 //some change by ar
 
