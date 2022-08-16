cff684fc97f24cedcae644866791b78f103ef921
Abdul Rehman Aziz <abdulrehmanx911@gmail.com>
2022-08-16 15:32:12 +0500

    pluralized files name to prevent confusion

--- src/components/Attire/CategoryDetail/CategoryDetail.tsx
+++ src/components/Attire/CategoryDetail/CategoryDetail.tsx
@@ -3,6 +3,42 @@ import "./CategoryDetail.css";
 import { useParams } from "react-router-dom";
 
 export const CategoryDetail = () => {
+  const arrayOfOpeningSentences = [
+    {
+      firstSentence: " Looking for some lit",
+      secondSentence: " ? Look no further",
+    },
+    {
+      firstSentence: "Here are some trendy ",
+      secondSentence: ". Enjoy",
+    },
+    {
+      firstSentence: "Some hot ",
+      secondSentence: "that caught our eyes",
+    },
+    {
+      firstSentence: "Our recommended ",
+      secondSentence: "just for you",
+    },
+    // {
+    //   firstSentence: "Some hot ",
+    //   secondSentence: "... ",
+    // },
+    // {
+    //   firstSentence: "The best clothes for",
+    //   secondSentence: "the best clothes for",
+    // },
+  ];
   const { name } = useParams();
-  return <div>This is the {name} category page</div>;
+  var random = Math.floor(Math.random() * arrayOfOpeningSentences.length);
+  return (
+    <div className="category-detail">
+      <h2>
+        {arrayOfOpeningSentences[random].firstSentence +
+          " " +
+          name +
+          arrayOfOpeningSentences[random].secondSentence}
+      </h2>
+    </div>
+  );
 };
